28d4c6356399ea5b6a90bd3c8432cb48f3257c6b
li ya hsuan <ab53014698@yahoo.com.tw>
2020-12-13 22:25:14 +0800

    新增路由+建立主樣板

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/tasks', [TaskController::class,'index']);
+Route::post('/task', [TaskController::class,'store']);
+Route::delete('/task/{task}', [TaskController::class,'destroy']);
